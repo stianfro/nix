5c5073e21e4c121c10cf968c06f8d7e5041b671e
Stian Frøystein <stianfroy@gmail.com>
2024-12-18 15:58:07 +0900

    feat: implement home-manager (#1)
    * feat: wip
    
    * feat: new structure
    
    * chore: move again
    
    * chore: move config back to flake
    
    * chore: move again
    
    * chore: move again
    
    * feat: split config
    
    * feat: add default config
    
    * chore: change first line
    
    * chore: add gitignore
    
    * feat: move home config
    
    * refactor: simplify output
    
    * refactor: move lines
    
    * feat: add home-manager
    
    * fix: solve weird path issue
    
    * feat: disable fish for now

---
+++ config/darwin.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+{
+  homebrew = {
+    enable = true;
+    brews = [ "mas" ];
+    casks = [
+      "firefox"
+      "linearmouse"
+      "wezterm"
+    ];
+    masApps = {
+      "Pure Paste" = 1611378436;
+    };
+    onActivation.cleanup = "zap";
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+  };
+
+  system.activationScripts.applications.text =
+    let
+      env = pkgs.buildEnv {
+        name = "system-applications";
+        paths = config.environment.systemPackages;
+        pathsToLink = "/Applications";
+      };
+    in
+    pkgs.lib.mkForce ''
+      echo "setting up /Applications..." >&2
+      rm -rf /Applications/Nix\ Apps
+      mkdir -p /Applications/Nix\ Apps
+      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read -r src; do
+        app_name=$(basename "$src")
+        echo "copying $src" >&2
+        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+    '';
+
+  system.defaults = {
+    dock.autohide = true;
+    dock.persistent-apps = [
+      "/Applications/Safari.app"
+      "/Applications/Firefox.app"
+      "/Applications/WezTerm.app"
+    ];
+    dock.show-recents = false;
+    finder.FXPreferredViewStyle = "clmv";
+    NSGlobalDomain.AppleICUForce24HourTime = true;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+    NSGlobalDomain.AppleInterfaceStyleSwitchesAutomatically = false;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Stian's darwin flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }: {
+    darwinConfigurations.m1 = nix-darwin.lib.darwinSystem {
+      modules = [
+        ./config/default.nix
+        ./config/darwin.nix
+
+        {
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          nix.settings.experimental-features = "nix-command flakes";
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "stianfroystein";
+            autoMigrate = true;
+          };
+
+          users.users.stianfroystein.home = "/Users/stianfroystein";
+
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.stianfroystein = import ./config/home.nix;
+
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+          system.stateVersion = 5;
+        }
+
+        home-manager.darwinModules.home-manager
+        nix-homebrew.darwinModules.nix-homebrew
+      ];
+    };
+  };
+}
