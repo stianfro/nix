2a301bb0a7da3c7d7b13b8304cb41e02ebeb6594
Stian Frøystein <stianfroy@gmail.com>
2024-12-18 15:34:56 +0900

    refactor: simplify output

--- flake.nix
+++ flake.nix
@@ -10,31 +10,25 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }:
-    let
-      revision = self.rev or self.dirtyRev or null;
-      config_default = import ./config/default.nix;
-      config_darwin = import ./config/darwin.nix;
-    in
-    {
-      darwinConfigurations.m1 = nix-darwin.lib.darwinSystem {
-        modules = [
-          config_default
-          config_darwin
-          nix-homebrew.darwinModules.nix-homebrew
-          {
-            nixpkgs.hostPlatform = "aarch64-darwin";
-            system.configurationRevision = revision;
-            nix-homebrew = {
-              enable = true;
-              enableRosetta = true;
-              user = "stianfroystein";
-              autoMigrate = true;
-            };
-            nix.settings.experimental-features = "nix-command flakes";
-            system.stateVersion = 5;
-          }
-        ];
-      };
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }: {
+    darwinConfigurations.m1 = nix-darwin.lib.darwinSystem {
+      modules = [
+        ./config/default.nix
+        ./config/darwin.nix
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nixpkgs.hostPlatform = "aarch64-darwin";
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "stianfroystein";
+            autoMigrate = true;
+          };
+          nix.settings.experimental-features = "nix-command flakes";
+          system.stateVersion = 5;
+        }
+      ];
     };
+  };
 }
