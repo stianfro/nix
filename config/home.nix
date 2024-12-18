771ef5d454513006781d14255e31dd3bfb126919
Stian Frøystein <stianfroy@gmail.com>
2024-12-18 15:57:43 +0900

    feat: disable fish for now

--- config/home.nix
+++ config/home.nix
@@ -73,4 +73,11 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # programs.fish = {
+  #   enable = true;
+  #   shellAliases = {
+  #     lg = "lazygit";
+  #   };
+  # };
 }
