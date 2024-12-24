b8a6fbf6e39cc6ce245237129c4ac70a83be8a9f
Stian Frøystein <stianfroy@gmail.com>
2024-12-24 15:20:34 +0900

    chore: rename file

--- config/home.nix
+++ config/home.nix
@@ -43,7 +43,7 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-    ".config/wezterm/wezterm.lua".source = "dotfiles/wezterm.lua";
+    ".config/wezterm/wezterm.lua".source = dotfiles/wezterm;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
