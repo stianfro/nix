5156852e366cf2a79b7fda7ae7847648e15b75ca
Stian Frøystein <stianfroy@gmail.com>
2024-12-26 13:01:25 +0900

    feat: add hammerspoon config

--- config/home.nix
+++ config/home.nix
@@ -47,6 +47,7 @@
     # ".screenrc".source = dotfiles/screenrc;
     ".config/wezterm/wezterm.lua".source = ./dotfiles/wezterm.lua;
     ".config/tmux/tmux.conf".source = ./dotfiles/tmux.conf;
+    ".hammerspoon/init.lua".source = ./dotfiles/hammerspoon.lua;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
