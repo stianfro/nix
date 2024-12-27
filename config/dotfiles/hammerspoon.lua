7d76ff9193e26ad18682443c68963e6ebdb5da2d
Stian Frøystein <stianfroy@gmail.com>
2024-12-27 20:06:11 +0900

    chore: autoformat

--- config/dotfiles/hammerspoon.lua
+++ config/dotfiles/hammerspoon.lua
@@ -1,67 +1,67 @@
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
-  hs.notify.new({ title = "Hammerspoon", informativeText = "Hello world!" }):send()
+	hs.notify.new({ title = "Hammerspoon", informativeText = "Hello world!" }):send()
 end)
 
 hs.hotkey.bind({ "cmd" }, "return", function()
-  hs.application.open("WezTerm")
+	hs.application.open("WezTerm")
 end)
 
-hs.hotkey.bind({ "cmd" }, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "H", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+	f.x = max.x
+	f.y = max.y
+	f.w = max.w / 2
+	f.h = max.h
+	win:setFrame(f)
 end)
 
-hs.hotkey.bind({ "cmd" }, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "L", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
 
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+	f.x = max.x + (max.w / 2)
+	f.y = max.y
+	f.w = max.w / 2
+	f.h = max.h
+	win:setFrame(f)
 end)
 
 -- not optimal
-hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "H", function()
-  local space = hs.spaces.focusedSpace()
-  local spaces = hs.spaces.spacesForScreen()
-
-  for i, x in pairs(spaces) do
-    if x == space then
-      S = i
-    end
-  end
-
-  local spaceleft = spaces[S - 1]
-
-  hs.spaces.gotoSpace(spaceleft)
-end)
-hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "L", function()
-  local space = hs.spaces.focusedSpace()
-  local spaces = hs.spaces.spacesForScreen()
-
-  for i, x in pairs(spaces) do
-    if x == space then
-      S = i
-    end
-  end
-
-  local spaceright = spaces[S + 1]
-
-  hs.spaces.gotoSpace(spaceright)
-end)
+-- hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "H", function()
+--   local space = hs.spaces.focusedSpace()
+--   local spaces = hs.spaces.spacesForScreen()
+--
+--   for i, x in pairs(spaces) do
+--     if x == space then
+--       S = i
+--     end
+--   end
+--
+--   local spaceleft = spaces[S - 1]
+--
+--   hs.spaces.gotoSpace(spaceleft)
+-- end)
+-- hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "L", function()
+--   local space = hs.spaces.focusedSpace()
+--   local spaces = hs.spaces.spacesForScreen()
+--
+--   for i, x in pairs(spaces) do
+--     if x == space then
+--       S = i
+--     end
+--   end
+--
+--   local spaceright = spaces[S + 1]
+--
+--   hs.spaces.gotoSpace(spaceright)
+-- end)
 
 hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "R", function()
-  hs.reload()
+	hs.reload()
 end)
