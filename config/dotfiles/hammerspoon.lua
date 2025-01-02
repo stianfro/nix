e56bfbd7ea7055651e2e71b10f816e45fac74168
Stian Frøystein <stianfroy@gmail.com>
2025-01-03 08:07:27 +0900

    feat: remove problematic hotkeys and add new

--- config/dotfiles/hammerspoon.lua
+++ config/dotfiles/hammerspoon.lua
@@ -1,42 +1,38 @@
-hs.application.enableSpotlightForNameSearches(true)
+hs.application.enablespotlightfornamesearches(true)
 
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
-	hs.notify.new({ title = "Hammerspoon", informativeText = "Hello world!" }):send()
-end)
+-- hs.hotkey.bind({ "cmd" }, "left", function()
+-- 	local win = hs.window.focusedWindow()
+-- 	local f = win:frame()
+-- 	local screen = win:screen()
+-- 	local max = screen:frame()
+--
+-- 	f.x = max.x
+-- 	f.y = max.y
+-- 	f.w = max.w / 2
+-- 	f.h = max.h
+-- 	win:setFrame(f)
+-- end)
+--
+-- hs.hotkey.bind({ "cmd" }, "right", function()
+-- 	local win = hs.window.focusedWindow()
+-- 	local f = win:frame()
+-- 	local screen = win:screen()
+-- 	local max = screen:frame()
+--
+-- 	f.x = max.x + (max.w / 2)
+-- 	f.y = max.y
+-- 	f.w = max.w / 2
+-- 	f.h = max.h
+-- 	win:setFrame(f)
+-- end)
 
-hs.hotkey.bind({ "cmd" }, "left", function()
-	local win = hs.window.focusedWindow()
-	local f = win:frame()
-	local screen = win:screen()
-	local max = screen:frame()
-
-	f.x = max.x
-	f.y = max.y
-	f.w = max.w / 2
-	f.h = max.h
-	win:setFrame(f)
-end)
-
-hs.hotkey.bind({ "cmd" }, "right", function()
-	local win = hs.window.focusedWindow()
-	local f = win:frame()
-	local screen = win:screen()
-	local max = screen:frame()
-
-	f.x = max.x + (max.w / 2)
-	f.y = max.y
-	f.w = max.w / 2
-	f.h = max.h
-	win:setFrame(f)
-end)
-
-hs.hotkey.bind({ "cmd" }, "up", function()
-	local win = hs.window.focusedWindow()
-	local screen = win:screen()
-	local max = screen:frame()
-
-	win:setFrame(max)
-end)
+-- hs.hotkey.bind({ "cmd" }, "up", function()
+-- 	local win = hs.window.focusedWindow()
+-- 	local screen = win:screen()
+-- 	local max = screen:frame()
+--
+-- 	win:setFrame(max)
+-- end)
 
 hs.hotkey.bind({ "cmd", "ctrl", "alt", "shift" }, "R", function()
 	hs.reload()
@@ -50,3 +46,9 @@ if app == nil or not app:isRunning() then
 		hs.reload()
 	end)
 end
+
+local hyper = { "cmd", "ctrl", "alt", "shift" }
+
+hs.hotkey.bind(hyper, "space", function()
+	hs.execute("yabai -m space --mirror y-axis")
+end)
