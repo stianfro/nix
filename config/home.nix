abf7a55af4602d6ba38242ad33351390d157d27c
Stian Frøystein <stianfroy@gmail.com>
2025-01-03 09:27:30 +0900

    feat: fix tmux vi cursor

--- config/home.nix
+++ config/home.nix
@@ -95,7 +95,16 @@
       vim = "nvim";
     };
     shellInit = ''
+      set fish_cursor_default block
+      set fish_cursor_insert line
+      set fish_cursor_replace_one underscore
+      set fish_cursor_visual block
+
       fish_vi_key_bindings
+
+      if set -q TMUX
+        fish_vi_cursor xterm
+      end
     '';
     functions = {
       gclone = ''
