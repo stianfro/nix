a41ce6c1e95226db0aad8d117f599371c5376b6b
Stian Frøystein <stianfroy@gmail.com>
2025-02-08 12:57:39 +0900

    refactor(hammerspoon): modify for colemak

--- config/dotfiles/hammerspoon.lua
+++ config/dotfiles/hammerspoon.lua
@@ -18,15 +18,15 @@ hs.hotkey.bind(hyper, "space", function()
 	hs.execute("/opt/homebrew/bin/yabai -m space --mirror y-axis")
 end)
 
-hs.hotkey.bind(hyper, "d", function()
+hs.hotkey.bind(hyper, "r", function()
 	hs.execute("/opt/homebrew/bin/yabai -m window --focus east")
 end)
 
-hs.hotkey.bind(hyper, "a", function()
+hs.hotkey.bind(hyper, "t", function()
 	hs.execute("/opt/homebrew/bin/yabai -m window --focus west")
 end)
 
-hs.hotkey.bind(hyper, "w", function()
+hs.hotkey.bind(hyper, "f", function()
 	hs.execute("/opt/homebrew/bin/yabai -m window --focus north")
 end)
 
